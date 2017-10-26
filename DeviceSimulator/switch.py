8341217bd359dc708d58fdab1c186b1b85d9d896
antonio <tonyldo@gmail.com>
2017-10-26 02:22:43 -0300

    Auto discovery works...

--- DeviceSimulator/switch.py
+++ DeviceSimulator/switch.py
@@ -8,7 +8,7 @@ from os import path
 CONFIG = {
     "broker": "127.0.0.1",
     "brokerPort": 1883,
-    "lastState": 0, 
+    "lastState": "OFF", 
     "client_id": "switchSimulator"+str(randint(0, 100000)),
     "configurationTopic": "homeassistant/switch/switchSimulator/config",
     "commandTopic": "home/lab/switchSimulator/set",
@@ -41,16 +41,32 @@ def on_connect(client, userdata, flags, rc):
         
         # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
-        client.subscribe("$SYS/#")
+        client.subscribe(CONFIG["commandTopic"])
     else:
         print("Bad connection Returned code=",rc)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
+    
+    if msg.payload==b'ON':
+       print ("send state: "+ "ON")
+       r=client.publish(CONFIG["stateTopic"],payload="ON",qos=2)
+       s = "RC:{} Mid{}"
+       print (s.format(*r))   
+    else:
+       print ("send state: "+ "OFF")
+       r=client.publish(CONFIG["stateTopic"],payload="OFF",qos=2)
+       s = "RC:{} Mid{}"
+       print (s.format(*r))    
+
 
 configLocation = path.dirname(__file__)+"/config.json"
 load_config(configLocation)
+
+state = CONFIG["lastState"]
+print("Last State "+ state)
+
 #create flag in class
 mqtt.Client.connected_flag=False 
 client = mqtt.Client(CONFIG["client_id"])
@@ -70,13 +86,16 @@ try:
         print("Waiting to connect...")
         time.sleep(1)
 
+    discoveryMsg = '{"name": "Switch Simulator", "command_topic": "' + CONFIG["commandTopic"] + '", "state_topic": "'+ CONFIG["stateTopic"] + '"}'
+    print (discoveryMsg)
+    r=client.publish(CONFIG["configurationTopic"],payload=discoveryMsg,qos=2)
     s = "RC:{} Mid{}"
-    r=client.publish(CONFIG["configurationTopic"],payload="teste2",qos=2)
     print (s.format(*r))
     
     #infinite loop for mantain the script alive
     while True:
         pass
+
 except KeyboardInterrupt:
     print ("\nBye...")
 finally:
